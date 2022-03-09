e1affddbc3e29b737ff61a56939ea5320692cfe9
Роман Медников <jellybe@yandex.ru>
2022-03-09 19:19:46 +0300

    Исправления в CORS.

--- src/general/cors.go
+++ src/general/cors.go
@@ -7,7 +7,7 @@ import (
 
 func SetupCORS(mux *http.ServeMux) http.Handler {
 	options := cors.Options{
-		AllowedOrigins:   []string{"http://localhost:80", "http://127.0.0.1:80", "http://localhost:3000", "http://127.0.0.1:3000"}, //"*"},
+		AllowedOrigins:   []string{"http://localhost:80", "http://127.0.0.1:80", "http://localhost:3000", "http://127.0.0.1:3000", "http://95.163.249.116:3000", "http://95.163.249.116:80"}, //"*"},
 		AllowedHeaders:   []string{"Origin", "Version", "Authorization", "Accept", "Accept-Encoding", "Content-Type", "Content-Length"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowCredentials: true,
