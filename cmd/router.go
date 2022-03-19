32b340eddb2edb2e5233b5145e780270ea1b314a
Роман Медников <jellybe@yandex.ru>
2022-03-19 10:38:56 +0300

    Исправления замечений.

--- cmd/router.go
+++ cmd/router.go
@@ -28,7 +28,7 @@ func (rm *RouterManager) NewRouter() http.Handler {
 	router := mux.NewRouter()
 	router.HandleFunc("/signin", middlewares.Middleware(rm.d.SignIn, false))
 	router.HandleFunc("/logout", middlewares.Middleware(rm.d.SignOut, true))
-	router.HandleFunc("/signup", middlewares.Middleware(rm.d.SignUp, true))
+	router.HandleFunc("/signup", middlewares.Middleware(rm.d.SignUp, false))
 	router.HandleFunc("/profile", middlewares.Middleware(rm.d.GetInfo, true))
 	router.HandleFunc("/income", middlewares.Middleware(rm.d.Income, true))
 	router.HandleFunc("/outcome", middlewares.Middleware(rm.d.Outcome, true))
