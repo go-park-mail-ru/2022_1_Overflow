255a790ef19991e65d937abf80ba1c42df09cb76
Роман Медников <jellybe@yandex.ru>
2022-03-20 10:47:33 +0300

    Исправлена валидация на почту.

--- internal/models/forms.go
+++ internal/models/forms.go
@@ -1,14 +1,14 @@
 package models
 
 type SignInForm struct {
-	Email		string	`json:"email" validate:"nonzero,max=20,regexp=^(?=[a-zA-Z0-9._]$)(?!.*[_.]{2})[^_.].*[^_.]$"`
+	Email		string	`json:"email" validate:"nonzero,max=20,regexp=^(?=[a-zA-Z0-9._]{1,20}$)(?!.*[_.]{2})[^_.].*[^_.]$"`
 	Password	string	`json:"password" validate:"nonzero,max=20"`
 }
 
 type SignUpForm struct {
 	FirstName		string	`json:"first_name" validate:"nonzero,max=20"`
 	LastName		string	`json:"last_name" validate:"nonzero,max=20"`
-	Email			string	`json:"email" validate:"nonzero,max=20,regexp=^(?=[a-zA-Z0-9._]$)(?!.*[_.]{2})[^_.].*[^_.]$"`
+	Email			string	`json:"email" validate:"nonzero,max=20,regexp=^(?=[a-zA-Z0-9._]{1,20}$)(?!.*[_.]{2})[^_.].*[^_.]$"`
 	Password		string	`json:"password" validate:"nonzero,max=20"`
 	PasswordConf	string	`json:"password_confirmation" validate:"nonzero,max=20"`
 }
