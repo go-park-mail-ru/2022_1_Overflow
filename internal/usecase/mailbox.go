9900b5e1ba729d69bde174c283b1daf19e615ba3
Роман Медников <jellybe@yandex.ru>
2022-04-14 18:25:55 +0300

    Исправление установки времени письма.

--- internal/usecase/mailbox.go
+++ internal/usecase/mailbox.go
@@ -149,6 +149,11 @@ func (uc *UseCase) SendMail(data *models.Session, form models.MailForm) pkg.Json
 	if (userAddressee == models.User{}) {
 		return pkg.NO_USER_EXIST
 	}
+	location, err := time.LoadLocation("Europe/Moscow")
+	if err != nil {
+		log.Error(err)
+		return pkg.INTERNAL_ERR
+	}
 	mail := models.Mail{
 		Client_id: user.Id,
 		Sender:    data.Username,
@@ -156,7 +161,7 @@ func (uc *UseCase) SendMail(data *models.Session, form models.MailForm) pkg.Json
 		Theme:     form.Theme,
 		Text:      form.Text,
 		Files:     form.Files,
-		Date:      time.Now(),
+		Date:      time.Now().In(location),
 	}
 	err = uc.db.AddMail(mail)
 	if err != nil {
