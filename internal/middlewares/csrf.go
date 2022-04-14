3600831485720d9d9610e6aa1ca81cfdc0167b3a
Роман Медников <jellybe@yandex.ru>
2022-04-14 14:33:26 +0300

    Добавление Access-Control-Expose-Headers для CSRF.

--- internal/middlewares/csrf.go
+++ internal/middlewares/csrf.go
@@ -14,6 +14,7 @@ func CSRFGetWrapper(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
 			w.Header().Set("X-CSRF-Token", csrf.Token(r))
+			w.Header().Set("Access-Control-Expose-Headers", "*")
 		}
 		handler.ServeHTTP(w, r)
 	})
