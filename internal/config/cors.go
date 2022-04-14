3ff4194da32738bd279e6e53e6cf325b4bd8b4e2
Роман Медников <jellybe@yandex.ru>
2022-04-14 14:57:25 +0300

    Исправление CORS.

--- internal/config/cors.go
+++ internal/config/cors.go
@@ -10,7 +10,7 @@ import (
 func SetupCORS(r *mux.Router) http.Handler {
 	options := cors.Options{
 		AllowedOrigins:   []string{"http://localhost:80", "http://127.0.0.1:80", "http://localhost:3000", "http://127.0.0.1:3000", "http://95.163.249.116:3000", "http://95.163.249.116:80"}, //"*"},
-		AllowedHeaders:   []string{"Origin", "Version", "Authorization", "Accept", "Accept-Encoding", "Content-Type", "Content-Length"},
+		AllowedHeaders:   []string{"Origin", "Version", "Authorization", "Accept", "Accept-Encoding", "Content-Type", "Content-Length", "X-CSRF-Token"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowCredentials: true,
 	}
