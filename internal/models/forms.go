1610d41876d098d1df6c91bba4c1fea333193f72
Роман Медников <jellybe@yandex.ru>
2022-04-22 12:17:10 +0300

    Изменен параметр max валидации для полей.

--- internal/models/forms.go
+++ internal/models/forms.go
@@ -1,16 +1,16 @@
 package models
 
 type SignInForm struct {
-	Username string `json:"username" validate:"nonzero,max=20,regexp=^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$"`
-	Password string `json:"password" validate:"nonzero,max=20"`
+	Username string `json:"username" validate:"nonzero,max=45,regexp=^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$"`
+	Password string `json:"password" validate:"nonzero,max=45"`
 }
 
 type SignUpForm struct {
-	FirstName    string `json:"first_name" validate:"nonzero,max=20"`
-	LastName     string `json:"last_name" validate:"nonzero,max=20"`
-	Username     string `json:"username" validate:"nonzero,max=20,regexp=^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$"`
-	Password     string `json:"password" validate:"nonzero,max=20"`
-	PasswordConf string `json:"password_confirmation" validate:"nonzero,max=20"`
+	FirstName    string `json:"first_name" validate:"nonzero,max=45"`
+	LastName     string `json:"last_name" validate:"nonzero,max=45"`
+	Username     string `json:"username" validate:"nonzero,max=45,regexp=^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$"`
+	Password     string `json:"password" validate:"nonzero,max=45"`
+	PasswordConf string `json:"password_confirmation" validate:"nonzero,max=45"`
 }
 
 type SettingsForm struct {
