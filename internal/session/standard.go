db6e6d277cf4eb84ae70ee9961eda534bc072a35
Роман Медников <jellybe@yandex.ru>
2022-04-26 01:49:57 +0300

    Исправление ошибки с сессиями.

--- internal/session/standard.go
+++ internal/session/standard.go
@@ -32,7 +32,7 @@ func (s *StandardManager) Init(config *config.Config) (err error) {
 		Secure:   false,
 	}
 
-	gob.Register(&utils_proto.Session{})
+	gob.Register(utils_proto.Session{})
 	return
 }
 
