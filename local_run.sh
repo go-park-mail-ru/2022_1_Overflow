c10c8fed2d4523e4dda139d374378bb5727e0a3c
Роман Медников <jellybe@yandex.ru>
2022-05-11 12:14:08 +0300

    Добавление логирования остальных сервисов в скрипт локального запуска.

--- local_run.sh
+++ local_run.sh
@@ -1,7 +1,7 @@
-(go run ./cmd/repository/run_repository.go &) && \
-(go run ./cmd/profile/run_profile.go &) && \
-(go run ./cmd/mailbox/run_mailbox.go &) && \
-(go run ./cmd/folder_manager/run_folder_manager.go &) && \
+(go run ./cmd/repository/run_repository.go > repository.log &) && \
+(go run ./cmd/profile/run_profile.go > profile.log &) && \
+(go run ./cmd/mailbox/run_mailbox.go > mailbox.log &) && \
+(go run ./cmd/folder_manager/run_folder_manager.go > folder_manager.log &) && \
 (go run ./cmd/auth/run_auth.go > auth.log &) && \
 (go run ./cmd/auth/run_smtp.go > smtp.log &) && \
 go run ./cmd/app/main.go
