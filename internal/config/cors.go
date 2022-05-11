a36f07ef59bb91272335066b3e8c9c58e0f936b4
Роман Медников <jellybe@yandex.ru>
2022-05-12 00:10:34 +0300

    Переконфигурация CORS для домена.

--- internal/config/cors.go
+++ internal/config/cors.go
@@ -9,7 +9,7 @@ import (
 
 func SetupCORS(r *mux.Router) http.Handler {
 	options := cors.Options{
-		AllowedOrigins:   []string{"http://localhost:80", "http://127.0.0.1:80", "http://localhost:3000", "http://127.0.0.1:3000", "http://95.163.249.116:3000", "http://95.163.249.116:80", "http://overmail.online:3000", "http://overmail.online:80"}, //"*"},
+		AllowedOrigins:   []string{"http://localhost:80", "http://127.0.0.1:80", "http://localhost:3000", "http://127.0.0.1:3000", "http://95.163.249.116:3000", "http://95.163.249.116:80", "http://overmail.online:3000", "http://overmail.online:80", "http://overmail.online"}, //"*"},
 		AllowedHeaders:   []string{"Origin", "Version", "Authorization", "Accept", "Accept-Encoding", "Content-Type", "Content-Length", "X-CSRF-Token"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowCredentials: true,
