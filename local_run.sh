7b0c58872263c2728a1ea8d258731afe1d049995
Роман Медников <jellybe@yandex.ru>
2022-05-11 12:15:39 +0300

    Различные исправления.

--- local_run.sh
+++ local_run.sh
@@ -3,5 +3,5 @@
 (go run ./cmd/mailbox/run_mailbox.go > mailbox.log &) && \
 (go run ./cmd/folder_manager/run_folder_manager.go > folder_manager.log &) && \
 (go run ./cmd/auth/run_auth.go > auth.log &) && \
-(go run ./cmd/auth/run_smtp.go > smtp.log &) && \
+(go run ./cmd/smtp/run_smtp.go > smtp.log &) && \
 go run ./cmd/app/main.go
